d07c3cfeb2d56ab30d7decbe8bd54f0cb3dc9560
guanfei <cn.guanfei@gmail.com>
2021-11-09 12:27:01 +0800

    no message

---
+++ src/Guest.vue
@@ -0,0 +1,64 @@
+<template>
+  <!-- Content Wrapper. Contains page content -->
+  <div class="login-box">
+    
+    <transition name="page" mode="out-in">
+      <router-view name="guest"></router-view>
+    </transition>
+    <br />
+
+    <div class="login-box-body">
+      <div class="site-index">
+        <a
+          href="/site/#collapseThree"
+          data-toggle="collapse"
+          data-parent="#accordion"
+          ><i class="fa fa-flag"></i> 当前进行工作</a
+        >
+      </div>
+
+      <div id="collapseThree" class="panel-collapse collapse">
+        <div class="panel-body">
+          <blockquote class="trello-board-compact">
+            <a href="https://trello.com/b/oxFuPkQy/" target="_blank"
+              >工作进度看板</a
+            >
+          </blockquote>
+        </div>
+      </div>
+      <hr />
+      <div class="site-index">
+        <a href="/static/public/files/BP@MrPP.pdf" target="_blank"><i class="fa fa-money"></i> 平台商业计划书</a>
+      </div>
+    </div>
+    <!-- /.login-box-body -->
+    <br/>
+    <div class="site-index"><a href="https://bujiaban.com" target="_blank"><i class="fa fa-home"></i> 上海不加班网络科技有限公司</a></div>
+    
+	  <div class="site-index"><a href="https://beian.miit.gov.cn/" target="_blank"><i class="fa fa-book"></i> 沪ICP备15039333号</a></div>
+    
+  </div>
+  <!-- /.login-box -->
+  <!-- /.content-wrapper -->
+</template>
+
+<script>
+export default {
+  name: 'guest-content-wrap',
+  script: null,
+  beforeCreate () {
+    document.querySelector('body').classList.add('login-page')
+    this.script = document.createElement('script')
+    this.script.type = 'text/javascript'
+    this.script.src = 'https://p.trellocdn.com/embed.min.js'
+    document.body.appendChild(this.script)
+  },
+  // 销毁前清除
+  beforeDestroy () {
+    document.querySelector('body').classList.remove('login-page')
+    document.body.removeChild(this.script)
+  },
+  created () {
+  }
+}
+</script>
--- src/main.js
+++ src/main.js
@@ -3,6 +3,47 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+import VueAxios from 'vue-axios'
+import axios from 'axios'
+import VueCookies from 'vue-cookies'
+
+Vue.use(ElementUI)
+Vue.use(VueCookies)
+Vue.$cookies.config('7d')
+Vue.use(VueAxios, axios)
+
+axios.interceptors.request.use(
+  function (config) {
+    if (store.state.api.access_token !== null) {
+      config.headers.Authorization = 'Bearer ' + store.state.api.access_token
+    }
+    return config
+  },
+  function (error) {
+    return Promise.reject(error)
+  }
+)
+axios.interceptors.response.use(
+  response => {
+    return response
+  },
+  error => {
+    if (error.response) {
+      switch (error.response.status) {
+        case 401:
+          // 返回 401 清除token信息并跳转到登录页面
+          confirm('过期')
+          router.replace({
+            path: '/'
+          })
+          location.reload()
+      }
+    }
+    return Promise.reject(error.response.data) // 返回接口返回的错误信息
+  })
+
 Vue.config.productionTip = false
 
 new Vue({
