f97174eada23837acd390031493dcc65c8e92d7f
guanfei <cn.guanfei@gmail.com>
2021-11-12 00:38:37 +0800

    no message

---
+++ src/components/Site.vue
@@ -0,0 +1,77 @@
+<template>
+  <div class="container">
+    <div class="main">
+      <div class="title">
+        <h4>混合现实编程平台</h4>
+      </div>
+      <!-- 手写开始 -->
+      <div class="box">
+        <div class="sub-title">Mixed Reality Programming Platform</div>
+
+        <slot></slot>
+      </div>
+      <!-- 手写结束 -->
+
+      <div class="box">增加内容</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+
+
+<style lang="scss" scoped>
+$bg:#2d3a4b;
+$dark_gray:#889aa4;
+$light_gray:#eee;
+
+.container {
+  min-height: 100%;
+  width: 100%;
+  background-color: #d3d6de;
+  overflow: hidden;
+ 
+  .main {
+    position: relative;
+    width: 500px;
+    max-width: 100%;
+    padding: 80px 35px 0;
+    margin: 0 auto;
+    overflow: hidden;
+  }
+  
+  .box {
+    position: relative;
+    height: 100%;
+    width: 500;
+    max-width: 100%;
+    padding: 10px 10px 10px 10px;
+    margin: 0 auto;
+    margin-bottom: 30px;
+    border-radius: 3px;
+    background-color: #fff;
+    overflow: hidden;
+  }
+
+  .title {
+    font-size: 35px;
+    color: #444444;
+    margin: 12px auto 30px auto;
+    text-align: center;
+    font-weight: bold;
+   
+  }
+  .sub-title {
+    font-size: 14px;
+    padding: 10px 10px 10px 10px;
+    text-align: center;
+    color: #666;
+  }
+
+
+
+}
+</style>
--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,344 +1,99 @@
 <template>
-  <div class="login-container">
-    <el-form
-      ref="loginForm"
-      :model="loginForm"
-      :rules="loginRules"
-      class="login-form"
-      auto-complete="on"
-      label-position="left"
-    >
-      <div class="title-container">
-        <h3 class="title">混合现实编程平台</h3>
+  <Site>
+    <div class="login-head">
+      <h1 class="login-welcome">欢迎!</h1>
+      <p class="login-text">准备好出发了么？</p>
+      <div class="login-link">
+        <el-link type="primary" :underline="false">登录账号 </el-link>
+        <br />
+        <el-link type="primary" :underline="false">注册用户 </el-link>
       </div>
-      <!-- 手写开始 -->
-      <div class="loginbox">
-        <div class="loginboxhead">
-          <div class="box-title">Mixed Reality Programming Platform</div>
-          <h1 class="box-welcome">欢迎!</h1>
-          <p class="box-text">准备好出发了么？</p>
-          <div class="box-link">
-            <el-link type="primary" :underline="false">登录账号 </el-link>
-            <br />
-            <el-link type="primary" :underline="false">注册用户 </el-link>
-          </div>
-        </div>
-        <hr />
-        <div class="loginboxbody">
-          <div class="box-hint">微信扫码，得到测试资格</div>
-          <img 
-          src="@/assets/qrcode.jpg" 
-          class="box-qrcode"
-          width="100%" 
-          alt="qrcode" 
-          />
-        </div>
-      </div>
-      <!-- 手写结束 -->
-      <el-form-item prop="username">
-        <span class="svg-container">
-          <svg-icon icon-class="user" />
-        </span>
-        <el-input
-          ref="username"
-          v-model="loginForm.username"
-          placeholder="Username"
-          name="username"
-          type="text"
-          tabindex="1"
-          auto-complete="on"
-        />
-      </el-form-item>
-
-      <el-form-item prop="password">
-        <span class="svg-container">
-          <svg-icon icon-class="password" />
-        </span>
-        <el-input
-          :key="passwordType"
-          ref="password"
-          v-model="loginForm.password"
-          :type="passwordType"
-          placeholder="Password"
-          name="password"
-          tabindex="2"
-          auto-complete="on"
-          @keyup.enter.native="handleLogin"
-        />
-        <span class="show-pwd" @click="showPwd">
-          <svg-icon
-            :icon-class="passwordType === 'password' ? 'eye' : 'eye-open'"
-          />
-        </span>
-      </el-form-item>
-
-      <el-button
-        :loading="loading"
-        type="primary"
-        style="width: 100%; margin-bottom: 30px"
-        @click.native.prevent="handleLogin"
-        >Login</el-button
-      >
-
-      <div class="tips">
-        <span style="margin-right: 20px">username: admin</span>
-        <span> password: any</span>
-      </div>
-    </el-form>
-  </div>
+    </div>
+    <hr />
+    <div class="login-body">
+      <div class="login-hint">微信扫码，得到测试资格</div>
+      <img
+        src="@/assets/qrcode.jpg"
+        class="login-qrcode"
+        width="100%"
+        alt="qrcode"
+      />
+    </div>
+  </Site>
 </template>
 
 <script>
-import { validUsername } from '@/utils/validate'
+// @ is an alias to /src
+import Site from '@/components/Site.vue'
 
 export default {
-  name: 'Login',
-  data() {
-    const validateUsername = (rule, value, callback) => {
-      if (!validUsername(value)) {
-        callback(new Error('Please enter the correct user name'))
-      } else {
-        callback()
-      }
-    }
-    const validatePassword = (rule, value, callback) => {
-      if (value.length < 6) {
-        callback(new Error('The password can not be less than 6 digits'))
-      } else {
-        callback()
-      }
-    }
-    return {
-      loginForm: {
-        username: 'admin',
-        password: '111111'
-      },
-      loginRules: {
-        username: [{ required: true, trigger: 'blur', validator: validateUsername }],
-        password: [{ required: true, trigger: 'blur', validator: validatePassword }]
-      },
-      loading: false,
-      passwordType: 'password',
-      redirect: undefined
-    }
-  },
-  watch: {
-    $route: {
-      handler: function(route) {
-        this.redirect = route.query && route.query.redirect
-      },
-      immediate: true
-    }
-  },
-  methods: {
-    showPwd() {
-      if (this.passwordType === 'password') {
-        this.passwordType = ''
-      } else {
-        this.passwordType = 'password'
-      }
-      this.$nextTick(() => {
-        this.$refs.password.focus()
-      })
-    },
-    handleLogin() {
-      this.$refs.loginForm.validate(valid => {
-        if (valid) {
-          this.loading = true
-          this.$store.dispatch('user/login', this.loginForm).then(() => {
-            this.$router.push({ path: this.redirect || '/' })
-            this.loading = false
-          }).catch(() => {
-            this.loading = false
-          })
-        } else {
-          console.log('error submit!!')
-          return false
-        }
-      })
-    }
+  name: 'LoginIndex',
+  components: {
+    Site
   }
 }
 </script>
 
-<style lang="scss">
-/* 修复input 背景不协调 和光标变色 */
-/* Detail see https://github.com/PanJiaChen/vue-element-admin/pull/927 */
 
-$bg:#283443;
-$light_gray:#fff;
-$cursor: #fff;
-
-@supports (-webkit-mask: none) and (not (cater-color: $cursor)) {
-  .login-container .el-input input {
-    color: $cursor;
-  }
-}
-
-/* reset element-ui css */
-.login-container {
-  .el-input {
-    display: inline-block;
-    height: 47px;
-    width: 85%;
-
-    input {
-      background: transparent;
-      border: 0px;
-      -webkit-appearance: none;
-      border-radius: 0px;
-      padding: 12px 5px 12px 15px;
-      color: $light_gray;
-      height: 47px;
-      caret-color: $cursor;
-
-      &:-webkit-autofill {
-        box-shadow: 0 0 0px 1000px $bg inset !important;
-        -webkit-text-fill-color: $cursor !important;
-      }
-    }
-  }
-
-  .el-form-item {
-    border: 1px solid rgba(255, 255, 255, 0.1);
-    background: rgba(0, 0, 0, 0.1);
-    border-radius: 5px;
-    color: #454545;
-  }
-}
-</style>
 
 <style lang="scss" scoped>
-$bg:#2d3a4b;
-$dark_gray:#889aa4;
-$light_gray:#eee;
+$bg: #2d3a4b;
+$dark_gray: #889aa4;
+$light_gray: #eee;
 
-.login-container {
-  min-height: 100%;
-  width: 100%;
-  background-color: #d3d6de;
-  overflow: hidden;
- 
-  .login-form {
-    position: relative;
-    width: 430px;
-    max-width: 100%;
-    padding: 80px 35px 0;
-    margin: 0 auto;
-    overflow: hidden;
-  }
-  
-  .loginbox {
-    position: relative;
-    height: 670px;
-    width: 430px;
-    max-width: 100%;
-    padding: 20px 20px 0;
-    margin: 0 auto;
-    margin-bottom: 30px;
-    border-radius: 2px;
-    background-color: #fff;
-    overflow: hidden;
-  }
+.login-head {
+  padding: 10px 10px 10px 10px;
+  max-width: 100%;
+}
 
-  .loginboxhead {
-    height:252px;
-    max-width: 100%;
-    // background-color: pink;
-  }
+.login-title {
+  font-size: 14px;
+  padding: 10px 10px 10px 10px;
+  text-align: center;
+  color: #666;
+}
 
-  .box-title {
-    font-size: 14px;
-    text-align: center;
-    color: #666;
-  }
+.login-welcome {
+  left: 20px;
+  top: 76px;
+  font-size: 36px;
+  font-weight: normal;
+  color: #666;
+}
 
-  .box-welcome {
-    position: absolute;
-    left: 20px;
-    top: 76px;
-    font-size: 36px;
-    font-weight: normal;
-    color: #666;
-  }
+.login-text {
+  left: 20px;
+  top: 152px;
+  font-size: 21px;
+  font-weight: lighter;
+  color: #666;
+}
 
-  .box-text {
-    position: absolute;
-    left: 20px;
-    top: 152px;
-    font-size: 21px;
-    font-weight: lighter;
-    color: #666;
-  }
+.login-link {
+  left: 20px;
+  top: 220px;
+}
 
-  .box-link {
-    position: absolute;
-    left: 20px;
-    top: 220px;
-  }
+.login-body {
+  padding-top: 14px;
+  height: 100%;
+  max-width: 100%;
+  padding: 10px 10px 20px 10px;
+  // background-color: rgb(127, 193, 219);
+}
 
-  .loginboxbody {
-    padding-top: 14px;
-    height:310px;
-    max-width: 100%;
-    // background-color: rgb(127, 193, 219);
-  }
+.login-hint {
+  font-size: 14px;
+  font-weight: lighter;
+  color: #666;
+  font-weight: bold;
+  // background-color: pink;
+}
 
-  .box-hint {
-    font-size: 14px;
-    font-weight: lighter;
-    color: #666;
-    font-weight: bold;
-    // background-color: pink;
-  }
-
-  .box-qrcode {
-    margin-top: 2px;
-    border: 1px solid;
-    border-radius: 4px;
-    border-color:rgb(213, 216, 216);
-  }
-
-  .tips {
-    font-size: 14px;
-    color: #fff;
-    margin-bottom: 10px;
-
-    span {
-      &:first-of-type {
-        margin-right: 16px;
-      }
-    }
-  }
-
-  .svg-container {
-    padding: 6px 5px 6px 15px;
-    color: $dark_gray;
-    vertical-align: middle;
-    width: 30px;
-    display: inline-block;
-  }
-
-  .title-container {
-    position: relative;
-
-    .title {
-      font-size: 35px;
-      color: #444444;
-      margin: 12px auto 30px auto;
-      text-align: center;
-      font-weight: bold;
-    }
-  }
-
-  .show-pwd {
-    position: absolute;
-    right: 10px;
-    top: 7px;
-    font-size: 16px;
-    color: $dark_gray;
-    cursor: pointer;
-    user-select: none;
-  }
+.login-qrcode {
+  margin-top: 2px;
+  border: 1px solid;
+  border-radius: 4px;
+  border-color: rgb(213, 216, 216);
 }
 </style>
