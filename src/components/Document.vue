2e30e6f1d211d36b7000832e4c6d999572ff2380
guanfei <cn.guanfei@gmail.com>
2021-12-31 16:46:29 +0800

    测试驱动开始

---
+++ src/components/Document.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    {{ count }}
+    <button @click="increment">自增</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name:'Document',
+  data() {
+    return {
+      count: 0
+    }
+  },
+
+  methods: {
+    increment() {
+      this.count++
+    }
+  }
+}
+</script>
--- tests/unit/mrpp/MrPP.spec.js
+++ tests/unit/mrpp/MrPP.spec.js
@@ -5,9 +5,12 @@ import Counter from '@/test/Counter.vue'
 describe('Counter.vue', () => {
   it('increments count when button is clicked', async() => {
     const wrapper = shallowMount(Counter)
+
+    expect(wrapper.findAll('div').exists()).toBe(true)
+    wrapper.find('button').trigger('click')
     wrapper.find('button').trigger('click')
     wrapper.find('button').trigger('click')
     await Vue.nextTick()
-    expect(wrapper.find('div').text()).toMatch('2')
+    expect(wrapper.find('div').text()).toMatch('3')
   })
 })
