e1ad72d4f51a2bc6408badf3e19383dd7c342bb4
delancy <34211046@qq.com>
2022-01-07 18:59:26 +0800

    论坛模块进行中 / 工作区间搭建进行中

---
+++ src/views/document/mains/Main1.vue
@@ -0,0 +1,16 @@
+
+<template>
+  <div>
+    main1
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Main1'
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/views/document/mains/Main2.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    main2
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Main2'
+}
+</script>
+
+<style>
+
+</style>
