06c6fe0d55ea2016803980a4e07227e6dc66d7c6
DESKTOP-00EU90Q\bjb <dirui@7dgame.com>
2022-01-13 12:09:37 +0800

    no message

--- dockerfile/shell/push.sh
+++ dockerfile/shell/push.sh
@@ -1,2 +1,2 @@
-docker tag mrpp-web-image dirui/mrpp-web:v0.20211118.1
-docker push dirui/mrpp-web:v0.20211118.1
+docker tag mrpp-web-image dirui/mrpp-web:v0.20220113.1
+docker push dirui/mrpp-web:v0.20220113.1
