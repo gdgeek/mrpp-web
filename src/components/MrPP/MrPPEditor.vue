67a528194568d332e9f2ddfed189e65f2762a394
dirui <dirui@bujiaban.com>
2022-01-17 18:06:34 +0800

    编辑器

---
+++ src/components/MrPP/MrPPEditor.vue
@@ -0,0 +1,77 @@
+<template>
+  <el-card class="box-card">
+    <el-divider content-position="left">发帖</el-divider>
+    <el-form ref="ruleForm" :model="ruleForm" :rules="rules" label-width="100px" class="demo-ruleForm">
+      <el-form-item label="标题" prop="name">
+        <el-input v-model="ruleForm.name" />
+      </el-form-item>
+
+      <el-form-item label="内容" prop="desc">
+        <froala id="edit" v-model="ruleForm.body" :tag="'textarea'" :config="config">1212</froala>
+
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary" @click="submitForm('ruleForm')">立即创建</el-button>
+        <el-button @click="resetForm('ruleForm')">重置</el-button>
+      </el-form-item>
+    </el-form>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+  </el-card>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      ruleForm: {
+        name: '',
+        body: ''
+      },
+      rules: {
+        name: [
+          { required: true, message: '请输入活动名称', trigger: 'blur' },
+          { min: 3, max: 5, message: '长度在 3 到 5 个字符', trigger: 'blur' }
+        ],
+        region: [
+          { required: true, message: '请选择活动区域', trigger: 'change' }
+        ],
+        date1: [
+          { type: 'date', required: true, message: '请选择日期', trigger: 'change' }
+        ],
+        date2: [
+          { type: 'date', required: true, message: '请选择时间', trigger: 'change' }
+        ],
+        type: [
+          { type: 'array', required: true, message: '请至少选择一个活动性质', trigger: 'change' }
+        ],
+        resource: [
+          { required: true, message: '请选择活动资源', trigger: 'change' }
+        ],
+        desc: [
+          { required: true, message: '请填写活动形式', trigger: 'blur' }
+        ]
+      }
+    }
+  },
+  methods: {
+    submitForm(formName) {
+      this.$refs[formName].validate((valid) => {
+        if (valid) {
+          alert('submit!')
+        } else {
+          console.log('error submit!!')
+          return false
+        }
+      })
+    },
+    resetForm(formName) {
+      this.$refs[formName].resetFields()
+    }
+  }
+}
+</script>
--- src/views/community/index.vue
+++ src/views/community/index.vue
@@ -1,63 +1,90 @@
 <template>
-  <div class="document-index">
-    <div style="height: 300px;">
-      <div class="block">
-        <el-timeline>
-          <el-timeline-item timestamp="2018/4/12" placement="top">
-            <el-card>
-              <h4>更新 Github 模板</h4>
-              <p>王小虎 提交于 2018/4/12 20:46</p>
-            </el-card>
-          </el-timeline-item>
-          <el-timeline-item timestamp="2018/4/3" placement="top">
-            <el-card>
-              <h4>更新 Github 模板</h4>
-              <p>王小虎 提交于 2018/4/3 20:46</p>
-            </el-card>
-          </el-timeline-item>
-          <el-timeline-item timestamp="2018/4/2" placement="top">
-            <el-card>
-              <h4>更新 Github 模板</h4>
-              <p>王小虎 提交于 2018/4/2 20:46</p>
-            </el-card>
-          </el-timeline-item>
-        </el-timeline>
-      </div>
-    </div>
+  <div id="CommuityIndex">
+    <el-container>
+
+      <br>
+      <el-header>
+        <el-card class="box-card">
+          <el-row :gutter="0">
+            <el-col :xs="16" :sm="16" :md="16" :lg="16" :xl="16">
+              <slot />
+              &nbsp;
+              1</el-col>
+            <el-col :xs="8" :sm="8" :md="8" :lg="8" :xl="8" />
+          </el-row>
+        </el-card>
+
+      </el-header>
+      <el-main>
+        <el-card class="box-card">
+          <el-table
+            :data="tableData"
+            style="width: 100%"
+          >
+            <el-table-column
+              prop="date"
+              label="日期"
+              width="180"
+            />
+            <el-table-column
+              prop="name"
+              label="姓名"
+              width="180"
+            />
+            <el-table-column
+              prop="address"
+              label="地址"
+            />
+          </el-table>
+        </el-card>
+
+      </el-main>
+    </el-container>
+    <mr-p-p-editor />
 
   </div>
 </template>
+
 <script>
-import { mapState, mapMutations } from 'vuex'
-import request from '@/utils/request'
+import MrPPEditor from '@/components/MrPP/MrPPEditor.vue'
+/*
+import FroalaEditor from 'froala-editor'
+FroalaEditor.DefineIcon('alert', { NAME: 'info', SVG_KEY: 'help' })
+FroalaEditor.RegisterCommand('alert', {
+  title: 'Hello',
+  focus: false,
+  undo: false,
+  refreshAfterCallback: false,
+  callback: function() {
+    alert('Hello!')
+  }
+})*/
 export default {
-  name: 'DocumentIndex',
+  name: 'CommuityIndex',
+  components: {
+    MrPPEditor
+  },
   data() {
     return {
-      title: '标题',
-      content: '载入中....',
-      date: 'Date: 2/10/2014'
+
+      tableData: [{
+        date: '2016-05-02',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1518 弄'
+      }],
+
+      config: {
+        attribution: false,
+        language: 'zh_cn',
+        //  toolbarButtons: [['bold',''], ['undo', 'redo', 'bold'], ['alert', 'clear', 'insert']],
+        events: {
+          initialized: function() {
+            console.log('initialized')
+          }
+        }
+      },
+      model: 'Edit Your Content Here!'
     }
-  },
-  computed: {
-    ...mapState({
-      document: state => state.document.index
-    })
-  },
-  created: function() {
-    // (this.$can('create', 'Post'))
-    request({
-      url: this.document + 'posts/999',
-      method: 'get'
-    }).then(response => {
-      this.title = response.data.title.rendered
-      this.content = response.data.content.rendered
-      this.date = response.data.date
-    })
-  },
-  methods: {
-    ...mapMutations(['setTitle', 'setSubTitle', 'setBreadcrumbs'])
   }
 }
 </script>
-
